22f9a256086237dd2293ee26259b8802f663ba18
iigmir <roc120j@gmail.com>
2023-12-10 01:37:58 +0800

    Rename

--- docs/index.js
+++ docs/index.js
@@ -42,19 +42,17 @@ const grid_selection = () => {
     doms.forEach( el => el.addEventListener( "click", main ) );
 };
 
-const answer_grid_by_using_panel = () => {
-    function render_current_dom(current_dom = Element, number = 0) {
+const update_grid_with_panel = () => {
+    const unfilled_answer = 0;
+    function update_current_dom(current_dom = Element, number = unfilled_answer) {
         current_dom.textContent = number;
         current_dom.classList.add("changable");
-        if (number === 0) {
+        if (number === unfilled_answer) {
             current_dom.textContent = "";
             current_dom.classList.remove("changable");
         }
     }
-    const current_dom_not_avaiable = (current_dom = Element) => [
-        !current_dom,
-        current_dom.classList.contains("filled"),
-    ].includes( true );
+    const current_dom_not_avaiable = (element) => !element || element.classList.contains("filled");
     const main = (ev) => {
         const number = Number(ev.target.value);
         const current_dom = document.querySelector(grid_app.current_grid_selector);
@@ -62,17 +60,17 @@ const answer_grid_by_using_panel = () => {
             return;
         }
         sudoku_app.set_element( grid_app.row - 1, grid_app.col - 1, number );
-        render_current_dom( current_dom, number );
+        update_current_dom( current_dom, number );
         document.querySelector(".app-panel .checked").textContent = JSON.stringify( sudoku_app.answer_checked );
     };
-    const doms = [...document.querySelectorAll("input[name=sudoku-num]")];
-    doms.forEach( radio => radio.addEventListener("change", main) );
+    const radio_inputs = [...document.querySelectorAll("input[name=sudoku-num]")];
+    radio_inputs.forEach( radio => radio.addEventListener("change", main) );
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
     sudoku_app.init_state(SUDOKU_EXAMPLE);
     render_questions( sudoku_app.question );
     grid_selection();
-    answer_grid_by_using_panel();
+    update_grid_with_panel();
 });
 

74535b40547bb8ed9fc6d961d612f651f1ec6a2e
iigmir <roc120j@gmail.com>
2023-12-10 01:46:54 +0800

    rewrite function

--- docs/index.js
+++ docs/index.js
@@ -32,17 +32,13 @@ const render_questions = (question = []) => {
 };
 
 
-const grid_selection = () => {
-    const main = (ev = MouseEvent) => {
-        const dom = ev.target;
-        grid_app.set_by_html(dom);
-        document.querySelector(".app-panel .info").textContent = `Row: ${grid_app.row}; Col: ${grid_app.col}`;
-    };
-    const doms = [...document.querySelectorAll("#app .item")];
-    doms.forEach( el => el.addEventListener( "click", main ) );
+const select_grid = (ev = MouseEvent) => {
+    const dom = ev.target;
+    grid_app.set_by_html(dom);
+    document.querySelector(".app-panel .info").textContent = `Row: ${grid_app.row}; Col: ${grid_app.col}`;
 };
 
-const update_grid_with_panel = () => {
+const update_grid_with_panel = (ev) => {
     const unfilled_answer = 0;
     function update_current_dom(current_dom = Element, number = unfilled_answer) {
         current_dom.textContent = number;
@@ -53,23 +49,24 @@ const update_grid_with_panel = () => {
         }
     }
     const current_dom_not_avaiable = (element) => !element || element.classList.contains("filled");
-    const main = (ev) => {
-        const number = Number(ev.target.value);
-        const current_dom = document.querySelector(grid_app.current_grid_selector);
-        if( current_dom_not_avaiable(current_dom) ) {
-            return;
-        }
-        sudoku_app.set_element( grid_app.row - 1, grid_app.col - 1, number );
-        update_current_dom( current_dom, number );
-    };
-    const radio_inputs = [...document.querySelectorAll("input[name=sudoku-num]")];
-    radio_inputs.forEach( radio => radio.addEventListener("change", main) );
+    const current_dom = document.querySelector(grid_app.current_grid_selector);
+    if( current_dom_not_avaiable(current_dom) ) {
+        return;
+    }
+    const number = Number(ev.target.value);
+    sudoku_app.set_element( grid_app.row - 1, grid_app.col - 1, number );
+    update_current_dom( current_dom, number );
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
     sudoku_app.init_state(SUDOKU_EXAMPLE);
     render_questions( sudoku_app.question );
-    grid_selection();
+    // Grid
+    const grids = [...document.querySelectorAll("#app .item")];
+    grids.forEach( el => el.addEventListener( "click", select_grid ) );
+    // Inputs
+    const radio_inputs = [...document.querySelectorAll("input[name=sudoku-num]")];
+    radio_inputs.forEach( radio => radio.addEventListener("change", update_grid_with_panel ) );
     update_grid_with_panel();
 });
 

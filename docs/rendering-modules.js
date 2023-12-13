8095e27ebc45bf93340efe02aab063ae4d7a8996
iigmir <roc120j@gmail.com>
2023-12-13 14:56:57 +0800

    okay

--- docs/rendering-modules.js
+++ docs/rendering-modules.js
@@ -1,19 +1,17 @@
-const set_grid_filled = (grid = Element) => {
-    grid.classList.add("filled");
-    grid.dataset.filled = true;
-};
-
 export const RenderGridText = (index_row) => (item, index_col) => {
     if (item > 0) {
         const grid_selector = `#app .item[data-row="${index_row + 1}"][data-col="${index_col + 1}"]`;
         const grid = document.querySelector(grid_selector);
         grid.textContent = item;
-        set_grid_filled(grid);
+        grid.classList.add("filled");
+        grid.dataset.filled = true;
     }
 };
 
 export const MarkIncorrectAnswerGrid = (legal = false, dom = Element) => {
     const grid_has_filled = (dom = Element) => Boolean(dom.dataset.filled) === true;
+    
+    // Action
     dom.classList.toggle("invalid", !legal);
     if (legal) {
         dom.classList.toggle("changable", !grid_has_filled(dom));
